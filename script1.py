978c6b2b1d2199a009122d6f360127ed2761b6e5
nikolaikryat <138504359+nikolaikryat@users.noreply.github.com>
2025-01-29 22:02:10 +0300

    Add files via upload

---
+++ script1.py
@@ -0,0 +1,7 @@
+import sys  # Загрузка библиотечного модуля
+
+print(sys.platform)
+print(2 ** 32)  # Возведение в степень
+x = 'Spam '
+print(x * 8)  # Повторение строки
+input('Press Enter to exit')
