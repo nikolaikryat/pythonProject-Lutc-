86ddd554eebb0d56feeba9efc0dc4130a763e3b1
Niko Kryat <nikolai.kryat@mail.ru>
2025-03-12 00:52:21 +0300

    matrix change

--- matrix_script.py
+++ matrix_script.py
@@ -1,8 +1,14 @@
 M = [[1, 2, 3],
      [4, 5, 6],
      [7, 8, 9]]
-print(M, '\n')
+print(M[0])
+print(M[1])
+print(M[2], '\n')
 
 diag = [M[i][i] for i in [0, 1, 2]]
-print(diag)
+print(diag, '\n')
 
+G = (sum(row) for row in M)
+print(next(G))
+print(next(G))
+print(next(G))
